fe9193da2cce73bd07a840c5a8a1af3fec495216
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-12-06 18:45:02 -0800

    add pink navbar and font family

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Navbar.scss'
 
 const Navbar = () => {
   return (
     <div>
       <nav>
         <li><Link to="/">About</Link></li>
+        <div>|</div>
+
         <li><Link to="/tech-stack">Tech Stack</Link></li>
+        <div>|</div>
+
         <li><Link to="/projects">Projects</Link></li>
+        <div>|</div>
+
         <li><Link to="/contact">Contact</Link></li>
       </nav>
     </div>
   )
 }
-
-
 
 export default Navbar
