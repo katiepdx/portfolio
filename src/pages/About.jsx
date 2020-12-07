7a46ebc79da700bd7e8b4d1483ac716cd9b0865b
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-12-06 19:31:09 -0800

    add about content and fonts

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './About.scss'
 
 export const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <section className='about-container'>
+      <div className='about-wrapper'>
+        <h1>Katie Perry</h1>
+        <section>
+          I am a full stack software developer with experience managing teams in a bilingual environment, developing curriculum for an overseas international school, and ESL teaching. Passionate about software creation and UI/UX design. I thrive in creative, fast-paced environments that involve collaboration and constant learning!
+      </section>
+      </div>
+    </section>
   )
 }
