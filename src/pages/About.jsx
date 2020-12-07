6c5a8ef06df13fda2a923192cc3cf4f9d3eeb71e
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-12-06 21:13:56 -0800

    center about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import './About.scss'
 export const About = () => {
   return (
     <section className='about-container'>
-      <div className='about-wrapper'>
+      <div className='about-content'>
         <h1>Katie Perry</h1>
         <section>
           I am a full stack software developer with experience managing teams in a bilingual environment, developing curriculum for an overseas international school, and ESL teaching. Passionate about software creation and UI/UX design. I thrive in creative, fast-paced environments that involve collaboration and constant learning!
