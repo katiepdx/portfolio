88727730bb5b0afac842318b82de8b6dddfb795f
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-12-10 14:33:43 -0800

    add academic adventurer image gallery slider

--- src/pages/Projects/AcademicAdvProject.jsx
+++ src/pages/Projects/AcademicAdvProject.jsx
@@ -3,7 +3,24 @@ import './Projects.scss'
 import githubIcon from '../../assets/github-icon.png'
 import linkIcon from '../../assets/link-icon.png'
 
+// image gallery slider imports
+import ImageGallery from 'react-image-gallery'
+import '../../../node_modules/react-image-gallery/styles/css/image-gallery.css'
+import welcome from '../../assets/academic-adventurer-assets/welcome.png'
+import pickPack from '../../assets/academic-adventurer-assets/pick-pack.png'
+import adventures from '../../assets/academic-adventurer-assets/adventures.png'
+import activity from '../../assets/academic-adventurer-assets/activity.png'
+import certificate from '../../assets/academic-adventurer-assets/certificate.png'
+
 export const AcademicAdvProject = () => {
+  const siteScreenshots = [
+    { original: welcome, thumbnail: welcome },
+    { original: pickPack, thumbnail: pickPack },
+    { original: adventures, thumbnail: adventures },
+    { original: activity, thumbnail: activity },
+    { original: certificate, thumbnail: certificate }
+  ]
+
   return (
     <section>
       <div>
@@ -24,8 +41,10 @@ export const AcademicAdvProject = () => {
           </a>
         </div>
       </div>
-      <div>
-        Project photo/video
+      <div className='image-gallery-slider'>
+        <ImageGallery
+          items={siteScreenshots}
+        />
       </div>
     </section>
   )
