0f8ea865fdee9321c9ab652d678e8b7b655c344a
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-02-02 13:41:15 -0800

    add fade in animations

--- src/pages/TechStack.jsx
+++ src/pages/TechStack.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../App.scss'
+import FadeIn from 'react-fade-in'
 
 export const TechStack = () => {
   return (
@@ -10,59 +11,69 @@ export const TechStack = () => {
         <section>
           {/* LANGUAGES */}
           <section>
-            <h2>Languages</h2>
-            <li>
-              <ul>JavaScript</ul>
-              <ul>HTML</ul>
-              <ul>CSS</ul>
-            </li>
+            <FadeIn>
+              <h2>Languages</h2>
+              <li>
+                <ul>JavaScript</ul>
+                <ul>HTML</ul>
+                <ul>CSS</ul>
+              </li>
+            </FadeIn>
           </section>
 
           {/* TESTING */}
           <section>
-            <h2>Testing</h2>
-            <li>
-              <ul>QUnit</ul>
-              <ul>Jest</ul>
-              <ul>Supertest</ul>
-            </li>
+            <FadeIn>
+              <h2>Testing</h2>
+              <li>
+                <ul>QUnit</ul>
+                <ul>Jest</ul>
+                <ul>Supertest</ul>
+              </li>
+            </FadeIn>
           </section>
         </section>
 
         {/* FRAMEWORKS/LIBRARIES */}
         <section>
-          <h2>Frameworks/<br></br>Libraries</h2>
-          <li>
-            <ul>React</ul>
-            <ul>Redux</ul>
-            <ul>Node</ul>
-            <ul>Express</ul>
-            <ul>PugJS</ul>
-            <ul>SCSS</ul>
-            <ul>socket.io</ul>
-          </li>
+          <FadeIn>
+            <h2>Frameworks/<br></br>Libraries</h2>
+            <li>
+              <ul>React</ul>
+              <ul>Redux</ul>
+              <ul>Node</ul>
+              <ul>Express</ul>
+              <ul>PugJS</ul>
+              <ul>SCSS</ul>
+              <ul>socket.io</ul>
+            </li>
+          </FadeIn>
         </section>
 
         <section>
           {/* DATABASE */}
           <section>
-            <h2>Database</h2>
-            <li>
-              <ul>PostgreSQL</ul>
-            </li>
+            <FadeIn>
+              <h2>Database</h2>
+              <li>
+                <ul>PostgreSQL</ul>
+              </li>
+            </FadeIn>
           </section>
 
           {/* DEV TOOLS */}
           <section>
-            <h2>Dev Tools</h2>
-            <li>
-              <ul>Github</ul>
-              <ul>Postman</ul>
-              <ul>PGAdmin</ul>
-              <ul>Heroku</ul>
-              <ul>Netlify</ul>
-              <ul>VSCode</ul>
-            </li>
+            <FadeIn>
+              <h2>Dev Tools</h2>
+              <li>
+                <ul>Github</ul>
+                <ul>Postman</ul>
+                <ul>PGAdmin</ul>
+                <ul>Heroku</ul>
+                <ul>Netlify</ul>
+                <ul>VSCode</ul>
+              </li>
+            </FadeIn>
           </section>
         </section>
       </section>
